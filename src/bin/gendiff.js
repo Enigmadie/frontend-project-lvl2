61c736b7178a21a329447d4448ea16441a06eb22
Enigmadie <gagen666@mail.ru>
2019-08-02 12:36:23 +0300

    edit bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,12 @@ import { version } from '../../package.json';
 
 program
   .version(version)
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+  });
 
 program.parse(process.argv);
